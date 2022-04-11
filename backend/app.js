b1077c0bcf48c68e56edf350eb6de38dd77acf9e
cans1994 <cans.bl@hotmail.com>
2022-04-11 15:15:36 +0200

    modification et suppression

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const app = express();
 
 const mongoose = require("mongoose");
 
-const Thing = require("./models/thing");
+const Thing = require("./models/Thing");
 
 app.use(express.json());
 
@@ -47,9 +47,22 @@ app.post("/api/stuff", (req, res, next) => {
 });
 
 app.get("/api/stuff/:id", (req, res, next) => {
+  //ceci affiche le détail du produit
   Thing.findOne({ _id: req.params.id })
     .then((thing) => res.status(200).json(thing))
     .catch((error) => res.status(404).json({ error }));
+});
+
+app.put("/api/stuff/:id", (req, res, next) => {
+  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: "Objet modifié !" }))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+app.delete("/api/stuff/:id", (req, res, next) => {
+  Thing.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: "Objet supprimé !" }))
+    .catch((error) => res.status(400).json({ error }));
 });
 
 module.exports = app;
