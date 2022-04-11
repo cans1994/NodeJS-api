85b964b47654b1698e9de768d298977541956092
cans1994 <cans.bl@hotmail.com>
2022-04-11 14:43:58 +0200

    ajout de tout les dossiers

---
+++ backend/app.js
@@ -0,0 +1,51 @@
+const express = require("express");
+
+const app = express();
+
+const mongoose = require("mongoose");
+
+const Thing = require("./models/thing");
+
+app.use(express.json());
+
+mongoose
+  .connect(
+    "mongodb+srv://cans1994:Erzincan2424@cluster0.nx0wh.mongodb.net/myFirstDatabase?retryWrites=true&w=majoritys",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.get("/api/stuff", (req, res, next) => {
+  Thing.find()
+    .then((things) => res.status(200).json(things))
+    .catch((error) => res.status(400).json({error }));
+});
+
+app.post("/api/stuff", (req, res, next) => {
+  delete req.body._id;
+  const thing = new Thing({
+    ...req.body, // ... => variable que l'on récupère dans les objets, synthétiser ttes les variables qu'il y a dedans
+  });
+  thing
+    .save()
+    .then(() => res.status(201).json({ message: "Objet enregistré !" }))
+    .catch((error) => res.status(400).json({ error })); // si le then crashe
+});
+
+
+
+module.exports = app;
